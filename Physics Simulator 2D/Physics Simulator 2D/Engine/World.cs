2f4abd1c4b55e6ddcf00524d03d25fe1cf62b1bc
Ian Myers <ianmyers52@gmail.com>
2013-10-09 16:51:18 -0600

    Created basic class files.

---
+++ Physics Simulator 2D/Physics Simulator 2D/Engine/World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics_Simulator_2D.Engine
+{
+    class World
+    {
+    }
+}
